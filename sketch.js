db7c4328a16b56d74b63f9e75094c5de94b8a2e5
MATHIAS-MAX <111917413+1bLEVI@users.noreply.github.com>
2023-11-10 16:26:21 -0300

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(1000, 800);
+   background(" white");
+}
+
+function draw() {
+   fill("red");
+   stroke("black")
+   strokeWeight(5);
+   if (mouseIsPressed){
+   rect(mouseX, mouseY, 20, 30,);
+ }
+}
